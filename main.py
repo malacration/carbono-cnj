a23339bd5752438cd65a9ece1fb9ee6eacef839e
Andrew May <andrewc3po@gmail.com>
2024-05-16 08:51:13 -0400

    Adicao de headless

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import os
 
 def getMetricas() :
     chrome_options=webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument("window-size=1400,2100") 
     chrome_options.add_argument('--disable-gpu')
